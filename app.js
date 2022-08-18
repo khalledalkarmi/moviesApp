127c592a2ee7c553e83003eb0b3f449d47f68d84
khalledalkarmi <khalled_95@hotmail.com>
2022-08-18 13:22:46 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const url = "http://www.omdbapi.com/?i=tt3896198&apikey=83a999a0";
+
+const search = "http://www.omdbapi.com/?t=" // replace whitespace with + 
+
+// fetch("http://www.omdbapi.com/?i=tt3896198&apikey=83a999a0").then((response) => response.json())
+// .then((data) => console.log(data));
+
+const searchButton = document.getElementById("searchButton")
+const searchFelid = document.getElementById("searchFelid")
+
+searchButton.onclick = (event) => {
+    event.preventDefault()
+    const moveName = searchFelid.value
+
+    getData(moveName);
+}
+
+function getData(moveName) {
+    moveName = moveName.replace(' ', '+');
+
+    fetch("http://www.omdbapi.com/?t=" + moveName + "&apikey=83a999a0")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+
+            display(data)
+        });
+}
+const main = document.getElementById("main");
+let cardDiv = document.getElementById("cardDiv");
+
+function display(data) {
+ 
+
+        let divCol = document.createElement("div");
+        divCol.className = "col-sm-3";
+        cardDiv.append(divCol);
+
+        let card = document.createElement("div");
+        card.className = "card"
+        divCol.append(card);
+
+        let imgCard = document.createElement("img");
+        imgCard.className = "card-img-top w-100";
+        imgCard.style.width = "100px"
+        imgCard.src = data.Poster
+        card.append(imgCard)
+
+        let cardBody = document.createElement("div");
+        cardBody.className = "card-body"
+        card.append(cardBody);
+
+        let cardTitle = document.createElement("h5");
+        cardTitle.className = "card-title"
+        cardTitle.textContent = data.Title
+        cardBody.append(cardTitle);
+
+        let cardText = document.createElement("p");
+        cardText.className = "card-text"
+        cardText.setAttribute('style', 'white-space: pre;');
+        cardText.textContent = "- Year: " + data.Year + " \n\r- Rated: " + data.Rated + "\n\r- Runtime: " + data.Runtime + "\n\r- Genre: " + data.Genre
+        cardBody.append(cardText);
+
+    }
+
+
+
+
